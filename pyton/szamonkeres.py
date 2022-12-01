78f149ac763af36c63f43251a7903dcd7edcc150
Héring István Attila <HeringIA@KKSZKI.LOCAL>
2022-12-01 13:26:06 +0100

    1,2,3 feladat kesz

--- pyton/szamonkeres.py
+++ pyton/szamonkeres.py
@@ -1,8 +1,32 @@
+# 1.feladat
 """"
 nev1 = input("kérem a vezetékneved:")
 nev2 = input("kérem a keresztneved:")
 print("Üdvoözöllek!")
 print(nev1,nev2)
 """
+# 2.feladat
 
-szam = input("kérek egy számot:")
+szam = int(input("kérek egy számot:"))
+print("A megelőző szám:",(szam - 1 ))
+print("A rákövetkező szám",(szam + 1))
+
+
+
+
+
+
+
+
+# 3.feladat
+"""
+szam1 = input("Kérem az első 1-100 között:")
+szam2 = input("Kérem a másik számot 1-100 között:")
+szam1 = int(szam1)
+szam2 = int(szam2)
+
+print("A két szám összege", szam1, "+", szam2, "=", szam1 + szam2)
+print("A két szám külömbsége", szam1, "-", szam2, "=", szam1 - szam2)
+print("A két szám szorzata", szam1, "*", szam2, "=", szam1 * szam2)
+print("A két szám hányadosa", szam1, "/", szam2, "=", szam1 / szam2)
+"""
